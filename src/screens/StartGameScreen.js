cc8e84785844e9c55b29dba614a923743c299335
padepokanpenguin <jamal.psw@gmail.com>
2023-01-13 09:41:52 +0700

    create a custom butotn

---
+++ src/screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
